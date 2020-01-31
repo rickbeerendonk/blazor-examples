057238d08de3cc0090e70886f9563ea1449bf8ba
Rick Beerendonk <rick@beerendonk.com>
2020-01-31 23:25:07 +0100

    Simplified "Simple" example.

--- 02 - Get Started/Simple/Startup.cs
+++ 02 - Get Started/Simple/Startup.cs
@@ -5,10 +5,6 @@ namespace Simple
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
         public void Configure(IComponentsApplicationBuilder app)
         {
             app.AddComponent<App>("app");
