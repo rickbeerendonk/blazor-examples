062f3071ee475c037b421d81a9d21da74fe55726
Rick Beerendonk <rick@beerendonk.com>
2020-02-08 12:18:26 +0100

    Add copyright.

--- 05 - List Rendering/ForEach/Program.cs
+++ 05 - List Rendering/ForEach/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
