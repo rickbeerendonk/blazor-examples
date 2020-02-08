df3ce6e36caf5e7a09fc1060e081b7990d9164f6
Rick Beerendonk <rick@beerendonk.com>
2020-02-08 12:11:46 +0100

    Add "Separate Template and Code Files - Basics" example.

---
+++ 04 - Separate Template and Code Files/a. Basics/App.razor.cs
@@ -0,0 +1,19 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace SeparateTempateAndCodeFiles_Basics
+{
+    public partial class App : ComponentBase
+    {
+
+        private int _count = 0;
+
+        private void handleClick(EventArgs e)
+        {
+            _count++;
+        }
+    }
+}
