8e004f6acb8afec26e4ac37d4704c6436cc6a371
Rick Beerendonk <rick@beerendonk.com>
2020-09-13 22:05:18 +0200

    Fix example.

--- 05 - Render Conditional/a. If/Program.cs
+++ 05 - Render Conditional/a. If/Program.cs
@@ -1,12 +1,8 @@
 ﻿/*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Blazor.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace RenderConditional_If
 {
@@ -15,6 +11,7 @@ namespace RenderConditional_If
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
             builder.RootComponents.Add<App>("app");
 
             await builder.Build().RunAsync();
