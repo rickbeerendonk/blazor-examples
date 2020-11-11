9d6f34233a44d483fc4b2122a9255c2fe40d3358
Rick Beerendonk <rick@beerendonk.com>
2020-11-11 15:02:01 +0100

    Add "Slots - Multiple" example.

---
+++ 09 - Slots/c. Multiple/wasm/Program.cs
@@ -0,0 +1,20 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Slots_Multiple
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
+            builder.RootComponents.Add<App>("#app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
