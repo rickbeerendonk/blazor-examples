d042cd05be151e52cad930f802d76a4434919ffe
Rick Beerendonk <rick@beerendonk.com>
2020-11-12 06:43:00 +0100

    Add "Render List - While" example.

---
+++ 06 - Render List/c. While/wasm/Program.cs
@@ -0,0 +1,19 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace RenderList_While
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
