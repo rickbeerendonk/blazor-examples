08077033d7c7b6242b4ad795ccaab9a2a4771650
Rick Beerendonk <rick@beerendonk.com>
2020-11-15 22:07:10 +0100

    Add Visual Basic version of example.

---
+++ 03 - Data Binding/1. Data to View/a. Content/wasm-vb/App.vb
@@ -0,0 +1,20 @@
+﻿' European Union Public License version 1.2
+' Copyright © 2020 Rick Beerendonk
+
+Imports System
+Imports Microsoft.AspNetCore.Components
+Imports Microsoft.AspNetCore.Components.Rendering
+
+Namespace DataBinding_DataToView_Content
+    Partial Public Class App : Inherits ComponentBase
+        Private name As String = "Blazor"
+
+        Protected Overrides Sub BuildRenderTree(builder As RenderTreeBuilder)
+            builder.OpenElement(0, "h1")
+            builder.AddContent(1, "Hello ")
+            builder.AddContent(2, name)
+            builder.AddContent(3, "!")
+            builder.CloseElement()
+        End Sub
+    End Class
+End Namespace
---
+++ 03 - Data Binding/1. Data to View/a. Content/wasm-vb/Program.vb
@@ -0,0 +1,14 @@
+﻿' European Union Public License version 1.2
+' Copyright © 2020 Rick Beerendonk
+
+Imports System.Threading.Tasks
+Imports Microsoft.AspNetCore.Components.WebAssembly.Hosting
+
+Module Program
+    Public Sub Main(args As String())
+        Dim builder = WebAssemblyHostBuilder.CreateDefault(args)
+        builder.RootComponents.Add(of DataBinding_DataToView_Content.App)("#app")
+
+        builder.Build().RunAsync()
+    End Sub
+End Module
