d0f3998e26eccd8e47d4144f025d4abff0acd2ee
Rick Beerendonk <rick@beerendonk.com>
2020-11-15 19:27:11 +0100

    Add "no-razor" version of example.

---
+++ 03 - Data Binding/1. Data to View/a. Content/wasm-no-razor/App.cs
@@ -0,0 +1,23 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace DataBinding_DataToView_Content
+{
+    public partial class App : ComponentBase
+    {
+        private string name = "Blazor";
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.OpenElement(0, "h1");
+            builder.AddContent(1, "Hello ");
+            builder.AddContent(2, name);
+            builder.AddContent(3, "!");
+            builder.CloseElement();
+        }
+    }
+}
