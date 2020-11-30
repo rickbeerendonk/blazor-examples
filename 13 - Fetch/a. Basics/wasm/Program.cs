54156fef5d5e9b5d910b208eca43e305e93ef999
Rick Beerendonk <rick@beerendonk.com>
2020-11-30 13:49:26 +0100

    Add "Fetch - Basics" example.

---
+++ 13 - Fetch/a. Basics/wasm/Program.cs
@@ -0,0 +1,25 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fetch_Basics
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            // Register HttpClient service
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
