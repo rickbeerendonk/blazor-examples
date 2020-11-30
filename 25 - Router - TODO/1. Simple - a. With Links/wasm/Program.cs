05f742eba3fb1acaa1acd601032f899d860f1129
Rick Beerendonk <rick@beerendonk.com>
2020-11-30 08:56:34 +0100

    Add "Router - Simple - With Links" example.

---
+++ 25 - Router - TODO/1. Simple - a. With Links/wasm/Program.cs
@@ -0,0 +1,20 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Router_Simple_WithLinks
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
+            builder.RootComponents.Add<App>("#app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
