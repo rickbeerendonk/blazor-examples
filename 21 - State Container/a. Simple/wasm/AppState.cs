9b5d666cd11f7aea649dca99bc6edf12e3763ef4
Rick Beerendonk <rick@beerendonk.com>
2021-01-31 16:33:23 +0100

    Add "State Container - Simple" example.

---
+++ 21 - State Container/a. Simple/wasm/AppState.cs
@@ -0,0 +1,22 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+using System;
+
+namespace StateContainer_Simple
+{
+    public class AppState
+    {
+        public string Color { get; private set; } = "red";
+
+        public event Action OnChange;
+
+        public void SwitchColor()
+        {
+            Color = (Color == "red") ? "green" : "red";
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
---
+++ 21 - State Container/a. Simple/wasm/Program.cs
@@ -0,0 +1,25 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StateContainer_Simple
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
+            builder.RootComponents.Add<Top>("#app");
+
+            // Add Services
+            builder.Services.AddSingleton<AppState>();
+
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
