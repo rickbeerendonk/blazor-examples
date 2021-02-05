01ac1630e2ec2ab5f0707cefaeebe32591ba8de9
Rick Beerendonk <rick@beerendonk.com>
2021-02-05 23:38:33 +0100

    Add "Forms - Input Controls - InputTextArea" example.

---
+++ 08 - Forms - TODO/b. Input Controls/02. InputTextArea/wasm/DemoModel.cs
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2021 Rick Beerendonk
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Forms_InputControls_InputTextArea
+{
+  public class DemoModel
+  {
+    public string Value { get; set; } = "Line 1\nLine 2";
+  }
+}
