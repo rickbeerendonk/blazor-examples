9be72ac9c7ad90adaefdebd1b6180030458d6883
Rick Beerendonk <rick@beerendonk.com>
2021-02-05 23:53:26 +0100

    Add "Forms - Input Controls - InputNumber" example.

---
+++ 08 - Forms - TODO/b. Input Controls/04. InputNumber/wasm/DemoModel.cs
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2021 Rick Beerendonk
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Forms_InputControls_InputNumber
+{
+  public class DemoModel
+  {
+    public int Value { get; set; } = 123;
+  }
+}
