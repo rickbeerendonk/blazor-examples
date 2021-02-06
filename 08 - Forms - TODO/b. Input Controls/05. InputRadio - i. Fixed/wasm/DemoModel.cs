a428890cb2397a7c1f7612c609d94ec0fd5dac45
Rick Beerendonk <rick@beerendonk.com>
2021-02-06 09:24:25 +0100

    Add "Forms - Input Controls - InputRadio - Fixed" example.

---
+++ 08 - Forms - TODO/b. Input Controls/05. InputRadio - i. Fixed/wasm/DemoModel.cs
@@ -0,0 +1,14 @@
+// European Union Public License version 1.2
+// Copyright © 2021 Rick Beerendonk
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Forms_InputControls_InputRadio_Fixed
+{
+    public enum Options { A, B, C };
+
+    public class DemoModel
+  {
+    public Options Value { get; set; }
+  }
+}
