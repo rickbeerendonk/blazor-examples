6c3b6c7aa51fb82cd3cb09fb9476d776889da92f
Rick Beerendonk <rick@beerendonk.com>
2021-02-07 21:10:48 +0100

    Add "Forms - Input Controls - InputSelect - Flexible" example.

---
+++ 08 - Forms - TODO/b. Input Controls/08. InputSelect - ii. Flexible/wasm/DemoModel.cs
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2021 Rick Beerendonk
+
+namespace Forms_InputControls_InputSelect_Flexible
+{
+  public enum Options { A, B, C }
+
+  public class DemoModel
+  {
+    public Options Value { get; set; }
+  }
+}
