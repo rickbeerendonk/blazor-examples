7e7354a235edb91759dd8ce875739609fc960d0b
Rick Beerendonk <rick@beerendonk.com>
2021-02-07 19:18:25 +0100

    Add "Forms - Input Controls - InputFile - Single" example.

---
+++ 08 - Forms - TODO/b. Input Controls/07. InputFile - i. Single/wasm/DemoModel.cs
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2021 Rick Beerendonk
+
+using System;
+
+namespace Forms_InputControls_InputFile_Single
+{
+  public class DemoModel
+  {
+    public DateTime Value { get; set; } = DateTime.Now;
+  }
+}
