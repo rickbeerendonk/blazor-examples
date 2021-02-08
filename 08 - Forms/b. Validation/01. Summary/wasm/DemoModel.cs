1b4ff2a8936294223b3f20e8245b65ef764daf45
Rick Beerendonk <rick@beerendonk.com>
2021-02-08 07:01:37 +0100

    Add range attribute.

--- 08 - Forms/b. Validation/01. Summary/wasm/DemoModel.cs
+++ 08 - Forms/b. Validation/01. Summary/wasm/DemoModel.cs
@@ -10,5 +10,9 @@ namespace Forms_Validation_Summary
     [Required]
     [StringLength(10, ErrorMessage = "Name is too long.")]
     public string Name { get; set; }
+
+    [Required]
+    [Range(18, 67, ErrorMessage = "Age is out of range.")]
+    public int Age { get; set; }
   }
 }
