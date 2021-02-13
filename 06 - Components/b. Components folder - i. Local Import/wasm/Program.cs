653aa7c8b24d080ad6b633243d7458266d152286
Rick Beerendonk <rick@beerendonk.com>
2021-02-13 22:56:37 +0100

    Add "Components - Components Folder - Global Import" example.

--- 06 - Components/b. Components folder - i. Local Import/wasm/Program.cs
+++ 06 - Components/b. Components folder - i. Local Import/wasm/Program.cs
@@ -4,7 +4,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Components_ComponentsFolder
+namespace Components_ComponentsFolder_LocalImport
 {
     public class Program
     {
