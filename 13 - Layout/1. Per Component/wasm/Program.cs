ce06dfb3077a3016cf28c36882ec9156ae6e16d3
Rick Beerendonk <rick@beerendonk.com>
2021-02-15 12:03:13 +0100

    Add "Layout - Shared" example.

--- 13 - Layout/1. Per Component/wasm/Program.cs
+++ 13 - Layout/1. Per Component/wasm/Program.cs
@@ -4,7 +4,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Layout_Basics
+namespace Layout_PerComponent
 {
     public class Program
     {
