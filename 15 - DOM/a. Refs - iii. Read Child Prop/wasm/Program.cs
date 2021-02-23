e7e0a712ddf0788da6228393973197877b96ff61
Rick Beerendonk <rick@beerendonk.com>
2021-02-23 22:06:43 +0100

    Add "DOM - Refs - Read Child Prop" example.

---
+++ 15 - DOM/a. Refs - iii. Read Child Prop/wasm/Program.cs
@@ -0,0 +1,20 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace DOM_Refs_ReadChildProp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
+            builder.RootComponents.Add<App>("#app");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
