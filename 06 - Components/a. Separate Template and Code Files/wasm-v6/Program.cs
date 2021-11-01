53952ed2ee1bdebda4eddf0e5c1fe6687d56f6dd
Rick Beerendonk <rick@beerendonk.com>
2021-11-02 00:49:26 +0100

    Add v6 version.

---
+++ 06 - Components/a. Separate Template and Code Files/wasm-v6/Program.cs
@@ -0,0 +1,10 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Components_SeparateTempateAndCodeFiles;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
