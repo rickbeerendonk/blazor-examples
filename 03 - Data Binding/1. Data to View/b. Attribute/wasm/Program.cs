5e4094fd3276e27a0289a60585cb573fe00521a4
Rick Beerendonk <rick@beerendonk.com>
2022-07-09 23:01:54 +0200

    Update to v6.

--- 03 - Data Binding/1. Data to View/b. Attribute/wasm/Program.cs
+++ 03 - Data Binding/1. Data to View/b. Attribute/wasm/Program.cs
@@ -1,19 +1,11 @@
 ﻿/*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace DataBinding_DataToView_Attribute
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+using DataBinding_DataToView_Attribute;
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+await builder.Build().RunAsync();
