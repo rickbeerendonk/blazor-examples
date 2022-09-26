33c7a695f34a87ac376aa8f602f5d5f9b6d33ebe
Rick Beerendonk <rick@beerendonk.com>
2022-09-26 21:26:45 +0200

    Update to v6.

--- 04 - Render Conditional/a. If/1. Elements/wasm/Program.cs
+++ 04 - Render Conditional/a. If/1. Elements/wasm/Program.cs
@@ -1,20 +1,10 @@
 ﻿/*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace RenderConditional_If_Elements
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            
-            builder.RootComponents.Add<App>("#app");
+using RenderConditional_If_Elements;
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
