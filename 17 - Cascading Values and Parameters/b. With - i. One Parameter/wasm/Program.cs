36ff158e177dc0770b06201373878d15c3d10250
Rick Beerendonk <rick@beerendonk.com>
2022-09-27 18:33:15 +0200

    Update to v6.

--- 17 - Cascading Values and Parameters/b. With - i. One Parameter/wasm/Program.cs
+++ 17 - Cascading Values and Parameters/b. With - i. One Parameter/wasm/Program.cs
@@ -1,20 +1,10 @@
 ﻿/*! European Union Public License version 1.2 !*/
 /*! Copyright © 2020 Rick Beerendonk          !*/
 
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace CascadingValuesParameters_With_OneParameter
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            
-            builder.RootComponents.Add<Top>("#app");
+using CascadingValuesParameters_With_OneParameter;
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Top>("#app");
+await builder.Build().RunAsync();
