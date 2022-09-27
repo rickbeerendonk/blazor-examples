17795a18485a4be2563d3ceb4740e060ff585e4b
Rick Beerendonk <rick@beerendonk.com>
2022-09-27 09:59:26 +0200

    Add WIP example.

---
+++ 07 - Component Interaction/2. Properties/c. Required - ii. Runtime [NET7] [WIP]/wasm/Program.cs
@@ -0,0 +1,9 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ComponentInteraction_Properties_Required;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
