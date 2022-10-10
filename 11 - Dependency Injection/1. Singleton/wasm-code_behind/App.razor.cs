21a472042f13b0b8e13df26bcee54cb72411b43b
Rick Beerendonk <rick@beerendonk.com>
2022-10-10 23:24:42 +0200

    Add variants.

---
+++ 11 - Dependency Injection/1. Singleton/wasm-code_behind/App.razor.cs
@@ -0,0 +1,20 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components;
+
+namespace DependencyInjection_Singleton_CodeBehind;
+
+public partial class App : ComponentBase
+{
+    // In  Blazor components:
+    // - Constructor injection is not possible
+    // - Inject only through properties
+    [Inject]
+    private LoggerService? Logger { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Logger?.Info("App component created.");
+    }
+}
---
+++ 11 - Dependency Injection/1. Singleton/wasm-code_behind/Program.cs
@@ -0,0 +1,14 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using DependencyInjection_Singleton_CodeBehind;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+// Add Services
+builder.Services.AddSingleton<LoggerService>();
+await builder.Build().RunAsync();
