aec2df4e97791ad53919704a1d8b699f59be4111
Rick Beerendonk <rick@beerendonk.com>
2022-10-11 16:39:24 +0200

    Remove unused code.

--- 12 - State Container/a. Simple/wasm/AppState.cs
+++ 12 - State Container/a. Simple/wasm/AppState.cs
@@ -1,22 +1,19 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-using System;
+namespace StateContainer_Simple;
 
-namespace StateContainer_Simple
+public class AppState
 {
-    public class AppState
+    public string Color { get; private set; } = "red";
+
+    public event Action? OnChange;
+
+    public void SwitchColor()
     {
-        public string Color { get; private set; } = "red";
-
-        public event Action? OnChange;
-
-        public void SwitchColor()
-        {
-            Color = (Color == "red") ? "green" : "red";
-            NotifyStateChanged();
-        }
-
-        private void NotifyStateChanged() => OnChange?.Invoke();
+        Color = (Color == "red") ? "green" : "red";
+        NotifyStateChanged();
     }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
 }
