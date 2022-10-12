a246b974a5f5f39c75a9d316f5587f270fe3f08e
Rick Beerendonk <rick@beerendonk.com>
2022-10-12 13:24:37 +0200

    Use Async in the name.

--- 16 - Fetch/b. Service/wasm/Todos/TodosService.cs
+++ 16 - Fetch/b. Service/wasm/Todos/TodosService.cs
@@ -5,35 +5,34 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
-namespace Fetch_Service
+namespace Fetch_Service;
+
+public class Todo
 {
-    public class Todo
-    {
-        public int UserID { get; set; }
+    public int UserID { get; set; }
 
-        public int ID { get; set; }
+    public int ID { get; set; }
 
-        public string? Title { get; set; }
+    public string? Title { get; set; }
 
-        public bool Completed { get; set; }
-    }
+    public bool Completed { get; set; }
+}
 
 #nullable enable
 
-    public interface ITodosService
+public interface ITodosService
+{
+    Task<Todo[]?> TodosAsync();
+}
+
+public class TodosHttpService : ITodosService
+{
+    private HttpClient http;
+
+    public TodosHttpService(HttpClient http) => this.http = http;
+
+    public Task<Todo[]?> TodosAsync()
     {
-        Task<Todo[]?> Todos();
-    }
-
-    public class TodosHttpService : ITodosService
-    {
-        private HttpClient http;
-
-        public TodosHttpService(HttpClient http) => this.http = http;
-
-        public Task<Todo[]?> Todos()
-        {
-            return http.GetFromJsonAsync<Todo[]>("https://jsonplaceholder.typicode.com/todos");
-        }
+        return http.GetFromJsonAsync<Todo[]>("https://jsonplaceholder.typicode.com/todos");
     }
 }
