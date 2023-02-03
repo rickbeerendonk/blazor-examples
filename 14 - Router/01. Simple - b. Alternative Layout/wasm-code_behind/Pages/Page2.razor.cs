12d4eb921cd2f02d8a03dfdf1bc3db6f45f02bd6
Rick Beerendonk <rick@beerendonk.com>
2023-02-03 23:32:41 +0100

    Add code-behind version.

---
+++ 14 - Router/01. Simple - b. Alternative Layout/wasm-code_behind/Pages/Page2.razor.cs
@@ -0,0 +1,14 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components;
+
+using Router_Simple_AltLayout_CodeBehind.Shared;
+
+namespace Router_Simple_AltLayout_CodeBehind.Pages;
+
+[LayoutAttribute(typeof(AltLayout))]
+[RouteAttribute("/page2")]
+public partial class Page2 : ComponentBase
+{
+}
