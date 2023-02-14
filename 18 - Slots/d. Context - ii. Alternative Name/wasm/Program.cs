995c334d001dc1aa4ab397877b855bf707cfc3d5
Rick Beerendonk <rick@beerendonk.com>
2023-02-15 00:25:43 +0100

    Add "Slots - Context - Alternative Name" example.

---
+++ 18 - Slots/d. Context - ii. Alternative Name/wasm/Program.cs
@@ -0,0 +1,10 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Slots_Context_AlternativeName;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
