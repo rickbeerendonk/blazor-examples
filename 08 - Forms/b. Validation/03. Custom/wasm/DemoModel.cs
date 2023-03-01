c031b2a4fb4e3c4f3f7e8fdd53d755763c26fc12
Rick Beerendonk <rick@beerendonk.com>
2023-03-01 09:42:24 +0100

    Add "Forms - Validation - Custom" example.

---
+++ 08 - Forms/b. Validation/03. Custom/wasm/DemoModel.cs
@@ -0,0 +1,11 @@
+// European Union Public License version 1.2
+// Copyright © 2023 Rick Beerendonk
+
+namespace Forms_Validation_Custom;
+
+public class DemoModel
+{
+    public string? First { get; set; }
+
+    public string? Second { get; set; }
+}
---
+++ 08 - Forms/b. Validation/03. Custom/wasm/Program.cs
@@ -0,0 +1,10 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Forms_Validation_Custom;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
