add63b0d31bf307e2905811fb9ce98154b6a456c
Rick Beerendonk <rick@beerendonk.com>
2023-03-02 12:29:24 +0100

    Add Basics tests.

---
+++ 24 - Testing/a. Basics/wasm/Test_Basic_Tests/AppCSharpTests.cs
@@ -0,0 +1,20 @@
+using Test_Basic;
+
+namespace Test_Basic_Tests;
+
+/// <summary>
+/// These tests are written entirely in C#.
+/// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
+/// </summary>
+public class AppCSsharpTests : TestContext
+{
+    [Fact]
+    public void ShouldRenderHelloBlazor()
+    {
+        // Arrange
+        var cut = RenderComponent<App>();
+
+        // Assert that content of the paragraph shows counter at zero
+        cut.Find("h1").MarkupMatches("<h1>Hello Blazor!</h1>");
+    }
+}
