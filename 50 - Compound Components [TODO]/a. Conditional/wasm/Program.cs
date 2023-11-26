31f5189228c332a864c4327d85ca00eb379b518a
Rick Beerendonk <rick@beerendonk.com>
2023-11-26 22:17:38 +0100

    Update NuGet versions.

---
+++ 50 - Compound Components [TODO]/a. Conditional/wasm/Program.cs
@@ -0,0 +1,10 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using RecursiveComponents_Conditional;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Greeting>("#app");
+await builder.Build().RunAsync();
