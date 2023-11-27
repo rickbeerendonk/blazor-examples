dfdf67963e003d574b767817a63d79ab600de500
Rick Beerendonk <rick@beerendonk.com>
2023-11-27 22:22:08 +0100

    Add "HeadContent - PageTitle" example.

---
+++ 15 - HeadContent/01. PageTitle/wasm/Program.cs
@@ -0,0 +1,14 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using HeadContent_PageTitle;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// 1. HeadOutlet
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
