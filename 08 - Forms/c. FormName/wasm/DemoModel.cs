89b11145790f00e522ee0fa3c29431465ac57c69
Rick Beerendonk <rick@beerendonk.com>
2024-02-01 15:24:58 +0100

    Add "Forms - FormName" example.

---
+++ 08 - Forms/c. FormName/wasm/DemoModel.cs
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2021 Rick Beerendonk
+
+namespace Forms_FormName
+{
+  public class DemoModel
+  {
+    public string Value { get; set; } = "Blazor";   
+  }
+}
