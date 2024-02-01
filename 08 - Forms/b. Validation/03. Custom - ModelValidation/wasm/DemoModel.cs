015a1348f42e8c8b7f5329ea1027a3b9c4eba650
Rick Beerendonk <rick@beerendonk.com>
2024-02-01 15:14:49 +0100

    Add "Forms - Validation - Custom - ModelValidation" example.

---
+++ 08 - Forms/b. Validation/03. Custom - ModelValidation/wasm/DemoModel.cs
@@ -0,0 +1,34 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Forms_Validation_Custom;
+
+[DemoValidation]
+public class DemoModel
+{
+    public string? First { get; set; }
+
+    public string? Second { get; set; }
+}
+
+internal class DemoValidationAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var model = validationContext.ObjectInstance as DemoModel;
+
+        if (model == null)
+            return new ValidationResult("Cannot validate input");
+
+        // Custom validation logic
+        if ((string.IsNullOrEmpty(model?.First) && string.IsNullOrEmpty(model?.Second))
+        || (!string.IsNullOrEmpty(model?.First) && !string.IsNullOrEmpty(model?.Second)))
+        {
+            return new ValidationResult("One of First or Second should have a value (not both).");
+        }
+
+        return ValidationResult.Success;
+    }
+}
