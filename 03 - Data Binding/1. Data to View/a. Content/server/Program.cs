01663b4afe9e99c2ee18f093f5716e1fa43c7ea0
Rick Beerendonk <rick@beerendonk.com>
2024-02-03 19:15:41 +0100

    Add server version.

---
+++ 03 - Data Binding/1. Data to View/a. Content/server/Program.cs
@@ -0,0 +1,17 @@
+using Simple.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
