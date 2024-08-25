55c6828fc6380f9beab63580f12742fc32714869
Rick Beerendonk <rick@beerendonk.com>
2024-08-25 11:43:46 +0200

    Update.

--- 08 - Forms/b. Validation/05. Translation [WIP]/wasm/Program.cs
+++ 08 - Forms/b. Validation/05. Translation [WIP]/wasm/Program.cs
@@ -1,8 +1,9 @@
 ﻿/*! European Union Public License version 1.2 !*/
 /*! Copyright © 2024 Rick Beerendonk          !*/
 
+using System.Globalization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Builder; 
+using Microsoft.Extensions.Localization;
 
 using Forms_Validation_Summary;
 using Forms_Validation_Summary.Resources;
@@ -12,15 +13,18 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 // Support translations
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddDataAnnotationsLocalization(options =>
-    {
-        options.DataAnnotationLocalizerProvider = (type, factory) =>
-            factory.Create(typeof(ValidationMessages));
-    });
+{
+    options.DataAnnotationLocalizerProvider = (type, factory) =>
+        factory.Create(typeof(ValidationMessages));
+});
 
 builder.RootComponents.Add<App>("#app");
 
-var app = builder.Build();
+var host = builder.Build();
 
-app.UseRequestLocalization(); // This will use the localization options configured above
+// Set the default culture for the application
+var defaultCulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentCulture = defaultCulture;
+CultureInfo.DefaultThreadCurrentUICulture = defaultCulture;
 
-await app.RunAsync();
+await host.RunAsync();
