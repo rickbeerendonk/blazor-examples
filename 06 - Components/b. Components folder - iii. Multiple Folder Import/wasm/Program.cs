749f3e4f8e567f50321af5292f6bbc7d0873d4f9
Rick Beerendonk <rick@beerendonk.com>
2024-08-26 22:50:46 +0200

    Add "Components - Components folder - Multiple Folder Import" example.

---
+++ 06 - Components/b. Components folder - iii. Multiple Folder Import/wasm/Program.cs
@@ -0,0 +1,10 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Components_ComponentsFolder_MultipleFolderImport;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
