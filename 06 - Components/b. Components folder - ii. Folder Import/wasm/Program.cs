efa119f40c6a77339cc8f8ac45850120a3dedb77
Rick Beerendonk <rick@beerendonk.com>
2024-08-26 22:16:22 +0200

    Improved name.

--- 06 - Components/b. Components folder - ii. Folder Import/wasm/Program.cs
+++ 06 - Components/b. Components folder - ii. Folder Import/wasm/Program.cs
@@ -3,7 +3,7 @@
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using Components_ComponentsFolder_GlobalImport;
+using Components_ComponentsFolder_FolderImport;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
