71bb5ce64962242d2f70998f4a8e7ab6c051b145
Rick Beerendonk <rick@beerendonk.com>
2024-08-29 03:09:27 +0200

    Add Basic Server + WASM WebApp.

---
+++ 60 - WebApp/BlazorApp_ServerAndWASM/BlazorApp_ServerAndWASM/BlazorApp_ServerAndWASM/Program.cs
@@ -0,0 +1,30 @@
+using BlazorApp_ServerAndWASM.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents()
+	.AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseWebAssemblyDebugging();
+}
+else
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode()
+	.AddInteractiveWebAssemblyRenderMode()
+	.AddAdditionalAssemblies(typeof(BlazorApp_ServerAndWASM.Client._Imports).Assembly);
+
+app.Run();
