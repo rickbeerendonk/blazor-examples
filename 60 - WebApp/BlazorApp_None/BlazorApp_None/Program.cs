149735564fcb593ecb852fa79f8ac23a4d3f05bd
Rick Beerendonk <rick@beerendonk.com>
2024-08-29 09:48:29 +0200

    Add WebApp None (rendermode) example.

---
+++ 60 - WebApp/BlazorApp_None/BlazorApp_None/Program.cs
@@ -0,0 +1,21 @@
+using BlazorApp_None.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
