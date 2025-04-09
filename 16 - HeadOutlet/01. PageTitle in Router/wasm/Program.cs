cb6bbc15505dbfc4202acc7ccf714455fc7dde1d
Rick Beerendonk <rick@beerendonk.com>
2025-04-09 20:17:13 +0200

    Add "HeadOutlet - PageTitle in Router" example.

---
+++ 16 - HeadOutlet/01. PageTitle in Router/wasm/Program.cs
@@ -0,0 +1,16 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+// 1. Namespace for HeadOutlet
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using PageTitle_In_Router;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// 1. HeadOutlet
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
