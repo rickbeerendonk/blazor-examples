57ad8deda3e59a2c1a89a4d60e93f7963516d517
Rick Beerendonk <rick@beerendonk.com>
2025-04-25 12:45:48 +0200

    Add MS Entra ID example.

---
+++ 27 - Security/a. Authentication/2. Entra ID/Demo/Program.cs
@@ -0,0 +1,17 @@
+using Demo;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Microsoft Authentication Library (MSAL) -- AzureAd = Azure Active Directory (now called Microsoft Entra ID)
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+});
+
+await builder.Build().RunAsync();
