0a86590d54d5a2a715875afde3650f73d8e3ccea
Rick Beerendonk <rick@beerendonk.com>
2025-04-25 03:44:06 +0200

    Rename.

--- 23 - QuickGrid/b. With/wasm/Program.cs
+++ 23 - QuickGrid/b. With/wasm/Program.cs
@@ -3,7 +3,7 @@
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using QuickGrid_Basics;
+using Demo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
