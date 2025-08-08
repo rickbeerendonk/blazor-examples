7fc63fbac84a9015a8af63c00e30dcfeccb196c4
Rick Beerendonk <rick@beerendonk.com>
2025-08-08 12:42:02 +0200

    Add dynamic form generation example

---
+++ 08 - Forms/d. Dynamic/wasm/DemoModel.cs
@@ -0,0 +1,12 @@
+// European Union Public License version 1.2
+// Copyright © 2024 Rick Beerendonk
+
+namespace Forms_Dynamic
+{
+  public class DemoModel
+  {
+    public string? Name { get; set; }
+    public string? Age { get; set; }
+    public string? City { get; set; }
+  }
+}
