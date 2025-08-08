180130009a6ec855688c2cd55640a723c0e8a6ef
Rick Beerendonk <rick@beerendonk.com>
2025-08-08 12:31:16 +0200

    Simplify localization sample without router

---
+++ 29 - Localization/Program.cs
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 */
+/*! Copyright © 2020 Rick Beerendonk */
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+
+using Localization;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+var supportedCultures = new[] { new CultureInfo("en"), new CultureInfo("nl") };
+CultureInfo.DefaultThreadCurrentCulture = supportedCultures[0];
+CultureInfo.DefaultThreadCurrentUICulture = supportedCultures[0];
+builder.Services.AddSingleton(supportedCultures);
+
+await builder.Build().RunAsync();
