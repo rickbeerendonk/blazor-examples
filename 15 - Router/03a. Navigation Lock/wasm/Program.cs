6806fe9fab2f02d666aed040bb9d92d200dadaef
Rick Beerendonk <rick@beerendonk.com>
2025-08-25 16:01:38 +0200

    Add "Router - Navigation Lock" demo.

---
+++ 15 - Router/03a. Navigation Lock/wasm/Program.cs
@@ -0,0 +1,10 @@
+﻿/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Router_NavigationLock;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+await builder.Build().RunAsync();
