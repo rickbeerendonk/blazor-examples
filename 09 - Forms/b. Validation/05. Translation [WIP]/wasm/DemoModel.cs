b76b1fdc6e18d46009118240eff82b8cbfac6a52
Rick Beerendonk <rick@beerendonk.com>
2025-08-27 01:33:40 +0200

    Update.

--- 09 - Forms/b. Validation/05. Translation [WIP]/wasm/DemoModel.cs
+++ 09 - Forms/b. Validation/05. Translation [WIP]/wasm/DemoModel.cs
@@ -1,19 +1,17 @@
 // European Union Public License version 1.2
-// Copyright © 2024 Rick Beerendonk
+// Copyright © 2020 Rick Beerendonk
 
 using System.ComponentModel.DataAnnotations;
+using Forms_Validation_Translation.Resources;
 
-using Forms_Validation_Summary.Resources;
+namespace Forms_Validation_Translation;
 
-namespace Forms_Validation_Summary
+public class DemoModel
 {
-    public class DemoModel
-    {
-        [Required(ErrorMessageResourceName = "NameRequired", ErrorMessageResourceType = typeof(ValidationMessages))]
-        [StringLength(10, ErrorMessageResourceName = "NameTooLong", ErrorMessageResourceType = typeof(ValidationMessages))]
-        public string? Name { get; set; }
+    [Required(ErrorMessageResourceName = "NameRequired", ErrorMessageResourceType = typeof(ValidationMessages))]
+    [StringLength(10, ErrorMessageResourceName = "NameTooLong", ErrorMessageResourceType = typeof(ValidationMessages))]
+    public string? Name { get; set; }
 
-        [Range(18, 67, ErrorMessageResourceName = "AgeOutOfRange", ErrorMessageResourceType = typeof(ValidationMessages))]
-        public int Age { get; set; }
-    }
+    [Range(18, 67, ErrorMessageResourceName = "AgeOutOfRange", ErrorMessageResourceType = typeof(ValidationMessages))]
+    public int Age { get; set; }
 }
--- 09 - Forms/b. Validation/05. Translation [WIP]/wasm/Program.cs
+++ 09 - Forms/b. Validation/05. Translation [WIP]/wasm/Program.cs
@@ -1,12 +1,9 @@
-﻿/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2024 Rick Beerendonk          !*/
+﻿// European Union Public License version 1.2
+// Copyright © 2020 Rick Beerendonk
 
 using System.Globalization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Localization;
-
-using Forms_Validation_Summary;
-using Forms_Validation_Summary.Resources;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -17,10 +14,10 @@ builder.RootComponents.Add<App>("#app");
 
 var host = builder.Build();
 
-// Set the default culture for the application
-var defaultCulture = new CultureInfo("en-US");
-var defaultUICulture = new CultureInfo("nl-NL");
-CultureInfo.DefaultThreadCurrentCulture = defaultCulture;
-CultureInfo.DefaultThreadCurrentUICulture = defaultUICulture;
+// Set the culture for the application
+// Change "nl-NL" to "en-US" for English
+var culture = new CultureInfo("nl-NL"); // Dutch by default
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 await host.RunAsync();
