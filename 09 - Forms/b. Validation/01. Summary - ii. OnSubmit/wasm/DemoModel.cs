a364038afcf47451e851bf12e2fe1992627ac2a3
Rick Beerendonk <rick@beerendonk.com>
2025-08-27 10:52:44 +0200

    Add "Forms - Validation - Summary - OnSubmit" example.

---
+++ 09 - Forms/b. Validation/01. Summary - ii. OnSubmit/wasm/DemoModel.cs
@@ -0,0 +1,16 @@
+// European Union Public License version 1.2
+// Copyright © 2020 Rick Beerendonk
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Forms_Validation_Summary_OnSubmit;
+
+public class DemoModel
+{
+    [Required]
+    [StringLength(10, ErrorMessage = "Name is too long.")]
+    public string? Name { get; set; }
+
+    [Range(18, 67, ErrorMessage = "Age is out of range.")]
+    public int Age { get; set; }
+}
