0e8e2ccd842049a2b2307bb986fb39b8f9fb1d5d
Rick Beerendonk <rick@beerendonk.com>
2026-03-11 01:58:00 +0100

    Modernize. Add WebApp version.

---
+++ 16 - Router/05. Query Params - a2. SingleReplace/webapp/Program.cs
@@ -0,0 +1,16 @@
+using Router_QueryParams_SingleReplace.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
